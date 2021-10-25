00e8ed6406ed0af75922f293ff8c964f7a96065a
Aleksey Makushin <m062tt@mail.ru>
2021-10-25 19:43:04 +0400

    Сборка второй день

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,11 @@
 import gulp from 'gulp';
+import config from './gulp/config';
 import clean from './gulp/tasks/clean';
 import server from './gulp/tasks/server';
-import config from './gulp/config';
+import { scriptsBuild, scriptsWatch } from './gulp/tasks/scripts';
 
 config.setEnv();
 
-export const build = gulp.series(
-  clean,
-);
+export const build = gulp.series(clean, scriptsBuild);
 
-export const watch = gulp.series(
-  build,
-  server,
-);
+export const watch = gulp.series(server, scriptsWatch);
