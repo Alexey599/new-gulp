04b098a00fa4cfc0cda30239764946fbee9407b5
Aleksey Makushin <m062tt@mail.ru>
2021-10-27 18:47:23 +0400

    Сборка день третий, настройка pug

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,9 +3,19 @@ import config from './gulp/config';
 import clean from './gulp/tasks/clean';
 import server from './gulp/tasks/server';
 import { scriptsBuild, scriptsWatch } from './gulp/tasks/scripts';
+import { pugBuild, pugWatch } from './gulp/tasks/pug';
 
 config.setEnv();
 
-export const build = gulp.series(clean, scriptsBuild);
+export const build = gulp.series(clean,
+  gulp.parallel(
+    scriptsBuild,
+    pugBuild,
+  ));
 
-export const watch = gulp.series(server, scriptsWatch);
+export const watch = gulp.series(build,
+  server,
+  gulp.parallel(
+    scriptsWatch,
+    pugWatch,
+  ));
