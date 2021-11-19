6781f723743f5c78218683799161b3daec92587b
Aleksey Makushin <m062tt@mail.ru>
2021-11-19 12:00:53 +0400

    Добавил миксин для шрифтов

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -1,10 +1,17 @@
 import gulp from 'gulp';
+import ttf2woff2 from 'gulp-ttf2woff2';
+import ttf2woff from 'gulp-ttf2woff';
 import config from '../config';
 
-const fontsBuild = () => (
+const fontsBuild = (ty) => {
   gulp.src(`${config.src.fonts}/**/*`)
-    .pipe(gulp.dest(config.dest.fonts))
-);
+    .pipe(ttf2woff())
+    .pipe(gulp.dest(config.dest.fonts));
+  gulp.src(`${config.src.fonts}/**/*`)
+    .pipe(ttf2woff2())
+    .pipe(gulp.dest(config.dest.fonts));
+  ty();
+};
 
 export const assetsBuild = gulp.parallel(fontsBuild);
 
